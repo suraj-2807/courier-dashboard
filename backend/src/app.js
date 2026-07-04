c3e4098e37999b8b23679e95d5ff2142a3b095f7
suraj-2807 <surajsabu2807@gmail.com>
2026-07-04 09:12:43 +0530

    Fix Express 5 catch-all route syntax using /*splat

--- backend/src/app.js
+++ backend/src/app.js
@@ -25,7 +25,7 @@ app.use('/api', routes)
 app.use(express.static(path.join(__dirname, '../dist')))
 
 // Fallback: serve React index.html for any frontend client routes
-app.get('*', (req, res) => {
+app.get('/*splat', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'))
 })
 
